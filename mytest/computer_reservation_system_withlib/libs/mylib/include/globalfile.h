ed2f1c75d0c1d8e1b2c1609512f1209693f8ab7a
michal chen <cqiang2020@163.com>
2023-06-24 21:58:30 +0800

    del_yuwen

--- mytest/computer_reservation_system_withlib/libs/mylib/include/globalfile.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-
-#define MANAGER_FILE   "../txt/manager.txt"// ./ 是指当前目录，也就是执行文件所在的目录，../是指当前目录的上级目录，也就是执行
-//文件所在目录的上一级目录，当前不是指这个头文件！
-#define TEACHER_FILE   "../txt/teacher.txt"
-#define STUDENT_FILE   "../txt/student.txt"
-#define COMPUTER_FILE  "../txt/computer.txt"
-#define ORDER_FILE     "../txt/order.txt"
--- mytest/eigentest/include/eigentest.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include<iostream>
-#include<stdio.h>
-#include<unistd.h>
-#include<string>
-#include<Eigen/Core>
-#include<Eigen/Dense>
-
-using namespace std;
